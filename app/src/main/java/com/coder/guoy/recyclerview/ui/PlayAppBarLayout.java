6ab9a962ba5078413ee101116ff89983961eb80f
KungFuDream <lengxuechiwu1314@163.com>
2017-04-25 20:22:37 +0800

    sample add coordinator appbarlayout collapsingtoolbar

--- app/src/main/java/com/coder/guoy/recyclerview/ui/PlayAppBarLayout.java
+++ app/src/main/java/com/coder/guoy/recyclerview/ui/PlayAppBarLayout.java
@@ -1,23 +1,32 @@
 package com.coder.guoy.recyclerview.ui;
 
 import android.graphics.Color;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
 
 import com.coder.guoy.recyclerview.R;
+import com.coder.guoy.recyclerview.utils.CommonUtils;
+import com.coder.guoy.recyclerview.view.statusbar.StatusBarUtil;
 
 public class PlayAppBarLayout extends AppCompatActivity {
+    private CoordinatorLayout mCoordinatorLayout;
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_app_bar_layout);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
         mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingtoolbar);
+
+        StatusBarUtil.setColor(PlayAppBarLayout.this, CommonUtils.getColor(R.color.colorTheme),0);
+
+        mCollapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
         //设置达到某一个状态之后的出现的效果
-        mCollapsingToolbarLayout.setContentScrimColor(Color.BLUE);
+        mCollapsingToolbarLayout.setContentScrimColor(CommonUtils.getColor(R.color.colorTheme));
         //设置达到某一个状态之后的状态栏出现的效果
-        mCollapsingToolbarLayout.setStatusBarScrimColor(Color.RED);//无效果
+        mCollapsingToolbarLayout.setStatusBarScrimColor(CommonUtils.getColor(R.color.colorTheme));//无效果
     }
 }
