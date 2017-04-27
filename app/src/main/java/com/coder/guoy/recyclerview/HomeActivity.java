10f97ed37e43af223211d4598c98bf27ca086041
KungFuDream <lengxuechiwu1314@163.com>
2017-04-27 18:17:09 +0800

    TextInputLayout add

--- app/src/main/java/com/coder/guoy/recyclerview/HomeActivity.java
+++ app/src/main/java/com/coder/guoy/recyclerview/HomeActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.coder.guoy.recyclerview.ui.PlayTextInputLayout;
 import com.coder.guoy.recyclerview.ui.statusbar.PlayStatusBar;
 import com.coder.guoy.recyclerview.ui.coordinator.PlayCoordinatorLayout;
 import com.coder.guoy.recyclerview.ui.animation.WelfareAnimationActivity;
@@ -36,6 +37,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.button5).setOnClickListener(this);
         findViewById(R.id.button6).setOnClickListener(this);
         findViewById(R.id.button7).setOnClickListener(this);
+        findViewById(R.id.button8).setOnClickListener(this);
     }
 
     @Override
@@ -64,6 +66,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.button7:
                 startActivity(new Intent(HomeActivity.this, PlayTabLayout.class));
+                break;
+            case R.id.button8:
+                startActivity(new Intent(HomeActivity.this, PlayTextInputLayout.class));
                 break;
         }
     }
---
+++ app/src/main/java/com/coder/guoy/recyclerview/ui/PlayTextInputLayout.java
@@ -0,0 +1,95 @@
+package com.coder.guoy.recyclerview.ui;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+
+import com.coder.guoy.recyclerview.R;
+import com.coder.guoy.recyclerview.base.MvvmBaseActivity;
+import com.coder.guoy.recyclerview.databinding.ActivityPlayTextInputLayoutBinding;
+import com.coder.guoy.recyclerview.utils.ToastUtil;
+
+public class PlayTextInputLayout extends MvvmBaseActivity<ActivityPlayTextInputLayoutBinding> implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_text_input_layout);
+        showContentView();
+        bindingView.back.setOnClickListener(this);
+        bindingView.btnLogin.setOnClickListener(this);
+        bindingView.textUsername.setHint("用户名/Email/手机号");
+        bindingView.textPassword.setHint("密码");
+        bindingView.edittextUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                username();
+            }
+        });
+        bindingView.edittextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                password();
+            }
+        });
+    }
+
+    private boolean username() {
+        if (TextUtils.isEmpty(bindingView.edittextUsername.getText())) {
+            bindingView.textUsername.setError("用户名/Email/手机号不能为空");
+            return false;
+        } else {
+            bindingView.textUsername.setError(null);
+        }
+        return true;
+    }
+
+    private boolean password() {
+        if (TextUtils.isEmpty(bindingView.edittextPassword.getText())) {
+            bindingView.textPassword.setError("密码不能为空");
+            return false;
+        } else {
+            bindingView.textPassword.setError(null);
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back:
+                finish();
+                break;
+            case R.id.btn_login:
+                if (username() && password()) {
+                    ToastUtil.show("登录成功");
+                } else {
+                    ToastUtil.show("登录失败");
+                }
+                break;
+        }
+    }
+
+}
