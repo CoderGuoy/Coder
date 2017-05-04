5989ab8d84d4bd73888a7e140319fd1128b23640
KungFuDream <lengxuechiwu1314@163.com>
2017-05-04 19:02:28 +0800

    animation add

---
+++ app/src/main/java/com/coder/guoy/recyclerview/ui/animation/PropertyAnimation.java
@@ -0,0 +1,20 @@
+package com.coder.guoy.recyclerview.ui.animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.coder.guoy.recyclerview.R;
+/**
+ * @Version:v1.0
+ * @Author:Guoy
+ * @CreateTime:2017年5月4日
+ * @Descrpiton:
+ */
+public class PropertyAnimation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_property_animation);
+    }
+}
