8fa8f629321aaac68c65497d554a4371d6bfe26a
KungFuDream <lengxuechiwu1314@163.com>
2017-05-10 18:22:22 +0800

    animation add

--- app/src/main/java/com/coder/guoy/recyclerview/ui/animation/PropertyAnimation.java
+++ app/src/main/java/com/coder/guoy/recyclerview/ui/animation/PropertyAnimation.java
@@ -1,8 +1,9 @@
 package com.coder.guoy.recyclerview.ui.animation;
 
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
 
 import com.coder.guoy.recyclerview.R;
 import com.coder.guoy.recyclerview.base.MvvmBaseActivity;
@@ -11,26 +12,25 @@ import com.coder.guoy.recyclerview.databinding.ActivityPropertyAnimationBinding;
 /**
  * @Version:v1.0
  * @Author:Guoy
- * @CreateTime:2017年5月4日
+ * @CreateTime:2017年5月10日
  * @Descrpiton:
  */
 public class PropertyAnimation extends MvvmBaseActivity<ActivityPropertyAnimationBinding> {
+
+    private WindowManager manager;
+    private int width;
+    private int height;
+    private float scalex = 1;
+    private float scaley = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property_animation);
+        manager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        width = manager.getDefaultDisplay().getWidth();
+        height = manager.getDefaultDisplay().getHeight();
+        Log.i("screen", "width:" + width + "height:" + height);
         showContentView();
-        initAnimation();
-    }
-
-    private void initAnimation() {
-        ObjectAnimator move = ObjectAnimator.ofFloat(bindingView.imageviewMove, "translationY", -200);
-        ObjectAnimator scaleX = ObjectAnimator.ofFloat(bindingView.imageviewMove, "scaleX", 0.5f, 1f);
-        ObjectAnimator scaleY = ObjectAnimator.ofFloat(bindingView.imageviewMove, "scaleY", 0.5f, 1f);
-        AnimatorSet set = new AnimatorSet();
-        set.setDuration(1000);
-        set.play(scaleX).with(scaleY);
-        set.start();
     }
 }
