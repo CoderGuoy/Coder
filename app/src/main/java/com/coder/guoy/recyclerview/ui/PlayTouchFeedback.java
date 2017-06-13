500fe45bbf7f988708fe8f3f5d548547f76fc219
CoderGuoy <lengxuechiwu1314@163.com>
2017-06-13 14:20:32 +0800

    Touch feedback update

--- app/src/main/java/com/coder/guoy/recyclerview/ui/PlayTouchFeedback.java
+++ app/src/main/java/com/coder/guoy/recyclerview/ui/PlayTouchFeedback.java
@@ -1,20 +1,31 @@
 package com.coder.guoy.recyclerview.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.coder.guoy.recyclerview.R;
+import com.coder.guoy.recyclerview.base.MvvmBaseActivity;
+import com.coder.guoy.recyclerview.databinding.ActivityPlayTouchFeedbackBinding;
+
 /**
  * @Version:v1.0
  * @Author:Guoy
  * @CreateTime:2017年6月12日
  * @Descrpiton:
  */
-public class PlayTouchFeedback extends AppCompatActivity {
+public class PlayTouchFeedback extends MvvmBaseActivity<ActivityPlayTouchFeedbackBinding> implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_touch_feedback);
+        showContentView();
+        bindingView.touchFeedback.textviewTitle.setText("Touch Feedback|触摸反馈");
+        bindingView.touchFeedback.toolbarBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        finish();
     }
 }
