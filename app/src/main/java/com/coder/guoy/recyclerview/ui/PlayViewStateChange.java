5c96d86e8bbbeac783f2f9d3185f5541c1103a17
CoderGuoy <lengxuechiwu1314@163.com>
2017-06-19 17:28:26 +0800

    view state change update

--- app/src/main/java/com/coder/guoy/recyclerview/ui/PlayViewStateChange.java
+++ app/src/main/java/com/coder/guoy/recyclerview/ui/PlayViewStateChange.java
@@ -1,17 +1,30 @@
 package com.coder.guoy.recyclerview.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.coder.guoy.recyclerview.R;
 import com.coder.guoy.recyclerview.base.MvvmBaseActivity;
 import com.coder.guoy.recyclerview.databinding.ActivityPlayViewStateChangeBinding;
-
-public class PlayViewStateChange extends MvvmBaseActivity<ActivityPlayViewStateChangeBinding> {
+/**
+ * @Version:v1.0
+ * @Author:Guoy
+ * @CreateTime:2017年6月19日
+ * @Descrpiton:
+ */
+public class PlayViewStateChange extends MvvmBaseActivity<ActivityPlayViewStateChangeBinding> implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_view_state_change);
         showContentView();
+        bindingView.viewstatechange.textviewTitle.setText("View State Changes|视图状态改变");
+        bindingView.viewstatechange.toolbarBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        finish();
     }
 }
