3e1fd080254c7321f807410287bd6529ce59f056
CoderGuoy <lengxuechiwu1314@163.com>
2017-06-20 18:28:21 +0800

    animated vector drawable add

---
+++ app/src/main/java/com/coder/guoy/recyclerview/ui/PlayAnimateVectorDrawables.java
@@ -0,0 +1,33 @@
+package com.coder.guoy.recyclerview.ui;
+
+import android.graphics.drawable.Animatable;
+import android.os.Bundle;
+import android.view.View;
+
+import com.coder.guoy.recyclerview.R;
+import com.coder.guoy.recyclerview.base.MvvmBaseActivity;
+import com.coder.guoy.recyclerview.databinding.ActivityPlayAnimateVectorDrawablesBinding;
+
+public class PlayAnimateVectorDrawables extends MvvmBaseActivity<ActivityPlayAnimateVectorDrawablesBinding> implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_animate_vector_drawables);
+        showContentView();
+        bindingView.animatevectordrawable.textviewTitle.setText("Animate Vector Drawables|矢量图片动画");
+        bindingView.animatevectordrawable.toolbarBack.setOnClickListener(this);
+        bindingView.imageSmile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.toolbar_back:
+                finish();
+                break;
+            case R.id.image_smile:
+                ((Animatable) bindingView.imageSmile.getDrawable()).start();
+        }
+    }
+}
--- app/src/main/java/com/coder/guoy/recyclerview/ui/holder/MaterialDesignHolder.java
+++ app/src/main/java/com/coder/guoy/recyclerview/ui/holder/MaterialDesignHolder.java
@@ -8,6 +8,7 @@ import com.coder.guoy.recyclerview.R;
 import com.coder.guoy.recyclerview.base.MvvmBaseActivity;
 import com.coder.guoy.recyclerview.databinding.ActivityMaterialDesignHolderBinding;
 import com.coder.guoy.recyclerview.ui.PlayActivityTransitions;
+import com.coder.guoy.recyclerview.ui.PlayAnimateVectorDrawables;
 import com.coder.guoy.recyclerview.ui.PlayCurvedMotion;
 import com.coder.guoy.recyclerview.ui.PlayFloatingActionButton;
 import com.coder.guoy.recyclerview.ui.PlayRevealEffect;
@@ -52,6 +53,7 @@ public class MaterialDesignHolder extends MvvmBaseActivity<ActivityMaterialDesig
         bindingView.btnCurved.setOnClickListener(this);
         bindingView.btnViewstate.setOnClickListener(this);
         bindingView.btnActivitytransition.setOnClickListener(this);
+        bindingView.btnVectordrawable.setOnClickListener(this);
     }
 
     @Override
@@ -107,6 +109,9 @@ public class MaterialDesignHolder extends MvvmBaseActivity<ActivityMaterialDesig
                 break;
             case R.id.btn_activitytransition:
                 startActivity(new Intent(MaterialDesignHolder.this, PlayActivityTransitions.class));
+                break;
+            case R.id.btn_vectordrawable:
+                startActivity(new Intent(MaterialDesignHolder.this, PlayAnimateVectorDrawables.class));
                 break;
         }
     }
